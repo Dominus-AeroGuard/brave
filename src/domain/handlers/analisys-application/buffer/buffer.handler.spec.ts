2149c603134ead82213beaf86d909022e6ede23a
mauriciofrancisco <mauricio.francisco@gmail.com>
2024-08-27 01:52:26 -0300

    add tests: buffer.handler and find-by-distance-protected-area.use-case

--- src/domain/handlers/analisys-application/buffer/buffer.handler.spec.ts
+++ src/domain/handlers/analisys-application/buffer/buffer.handler.spec.ts
@@ -1,8 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BufferHandler } from './buffer.handler';
+import { IProtectedAreaTypeRepository } from '../../../../infra/prisma/repositories/protected-area-type.repository';
+import { IProtectedAreaRepository } from '../../../../infra/prisma/repositories/protected-area.repository';
+import { IApplicationAnalisysRepository } from '../../../../infra/prisma/repositories/application-analisys.repository';
+import { ApplicationAnalisysStatusEnum } from '../../../../domain/enums/application-analisys-status.enum';
+import { ApplicationAnalisysTypeEnum } from '../../../../domain/enums/application-analisys-type.enum';
 
 describe('BufferHandler', () => {
   let service: BufferHandler;
+  let protectedAreaRepository: IProtectedAreaRepository;
+  let protectedAreaTypeRepository: IProtectedAreaTypeRepository;
+  let analisysRepository: IApplicationAnalisysRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -30,9 +38,38 @@ describe('BufferHandler', () => {
     }).compile();
 
     service = module.get<BufferHandler>(BufferHandler);
+    protectedAreaRepository = module.get<IProtectedAreaRepository>(
+      'IProtectedAreaRepository',
+    );
+    protectedAreaTypeRepository = module.get<IProtectedAreaTypeRepository>(
+      'IProtectedAreaTypeRepository',
+    );
+    analisysRepository = module.get<IApplicationAnalisysRepository>(
+      'IApplicationAnalisysRepository',
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('handle', () => {
+    it('should execute buffer analysis', async () => {
+      const areaTypes = [
+      ];
+     
+      const repositorySpy = {
+        findAll: jest
+          .spyOn(protectedAreaTypeRepository, 'findAll')
+          .mockResolvedValue(areaTypes),
+      };
+
+      const context = {
+        applicationId: BigInt(1),
+        userId: 1
+      }
+
+      // Act
+      const result = await service.handle(context);
+
+      expect(result).toEqual(null);
+    });
   });
+
 });
---
+++ src/domain/use-cases/protected-area/find-by-distance-protected-area.use-case.spec.ts
@@ -0,0 +1,52 @@
+import { Test } from '@nestjs/testing';
+import { IProtectedAreaRepository } from '../../../infra/prisma/repositories/protected-area.repository';
+import { Readable } from 'stream';
+import { FindByDistanceProtectedAreaUseCase } from './find-by-distance-protected-area.use-case';
+import { Application } from 'src/domain/entities';
+
+describe('FindByDistanceProtectedAreaUseCase', () => {
+  let useCase: FindByDistanceProtectedAreaUseCase;
+  let repository: IProtectedAreaRepository;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        FindByDistanceProtectedAreaUseCase,
+        {
+          provide: 'IProtectedAreaRepository',
+          useValue: {
+            findByDistance: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    useCase = moduleRef.get<FindByDistanceProtectedAreaUseCase>(
+      FindByDistanceProtectedAreaUseCase,
+    );
+    repository = moduleRef.get<IProtectedAreaRepository>(
+      'IProtectedAreaRepository',
+    );
+  });
+
+  describe('execute', () => {
+    it('should find protected areas by distance from application', async () => {
+      const areas = [
+      ];
+     
+      const repositorySpy = {
+        findByDistance: jest
+          .spyOn(repository, 'findByDistance')
+          .mockResolvedValue(areas),
+      };
+
+      // Act
+      const result = await useCase.execute({applicationId: 1, distance: 500, typeId: 2});
+
+      // Assert
+      expect(repositorySpy.findByDistance).toHaveBeenCalledWith(1, 500, 2);
+
+      expect(result).toEqual([]);
+    });
+  });
+});
