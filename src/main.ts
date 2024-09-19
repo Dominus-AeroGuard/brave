f721923e6933b03bb5634ed7957f1220a0953cc5
Samuel Padua <saamuel.faria@gmail.com>
2024-09-19 10:48:17 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,11 @@ import {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('AeroGuard API')
